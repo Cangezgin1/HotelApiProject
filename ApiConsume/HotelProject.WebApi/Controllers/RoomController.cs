938d057e975fe3e180736289e8489b1c890dfff4
can.gezgin <can.gezgin@WRPNW064>
2023-05-03 17:03:30 +0300

    Staff ve Room için api controllerlar oluşturuldu.

---
+++ ApiConsume/HotelProject.WebApi/Controllers/RoomController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult RoomList() // Tüm room ların gelmesi için
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult AddRoom() // Room eklemek için
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteRoom() // Room silmek için
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateRoom() // Room güncelleme için
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetRoom() // id'ye göre room getirmek için
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ ApiConsume/HotelProject.WebApi/Controllers/StaffController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult StaffList() // Tüm Staff ların gelmesi için
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult AddStaff() // Staff eklemek için
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteStaff() // Staff silmek için
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateStaff() // Staff güncelleme için
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStaff() // id'ye göre Staff getirmek için
+        {
+            return Ok();
+        }
+    }
+}
