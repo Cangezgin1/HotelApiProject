facbad7f97814a5b6511143c1c25d9a2b2863192
can.gezgin <can.gezgin@WRPNW064>
2023-05-03 16:00:16 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfServiceDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrate;
+using HotelProject.DataAccessLayer.Repository;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.DataAccessLayer.EntityFramework
+{
+    public class EfServiceDal : GenericRepository<Service>,IServicesDal
+    {
+        public EfServiceDal(Context context):base(context)
+        {
+
+        }
+    }
+}
---
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfTestimonialDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrate;
+using HotelProject.DataAccessLayer.Repository;
+using HotelProject.EntityLayer.Concrete;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.EntityFramework
+{
+    public class EfTestimonialDal:GenericRepository<Testimonial>,ITestimonialDal
+    {
+        public EfTestimonialDal(Context context):base(context)
+        {
+
+        }
+    }
+}
