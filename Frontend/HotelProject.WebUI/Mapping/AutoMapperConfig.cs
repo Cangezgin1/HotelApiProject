77b5d93f1b905c4998002ceb32c3a87efb1055e0
can.gezgin <can.gezgin@WRPNW064>
2023-05-15 16:05:32 +0300

    Service CRUD finished with Dtos

---
+++ Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.ServiceDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Mapping
+{
+    public class AutoMapperConfig:Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<ResultServiceDto, Service>().ReverseMap();
+            CreateMap<UpdateServiceDto, Service>().ReverseMap();
+            CreateMap<CreateServiceDto, Service>().ReverseMap();
+        }
+    }
+}
