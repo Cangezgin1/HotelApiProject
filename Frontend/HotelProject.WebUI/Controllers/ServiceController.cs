bdef745de0499ef6435bf826a41bfa92d66e2a5a
Muhammed Can GEZGİN <93928479+Cangezgin1@users.noreply.github.com>
2024-01-30 17:48:29 +0300

    Update ServiceController.cs

--- Frontend/HotelProject.WebUI/Controllers/ServiceController.cs
+++ Frontend/HotelProject.WebUI/Controllers/ServiceController.cs
@@ -8,7 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HotelProject.WebUI.Controllers
+namespace HotelProject.WebUI.Controllers 
 {
     public class ServiceController : Controller
     {
