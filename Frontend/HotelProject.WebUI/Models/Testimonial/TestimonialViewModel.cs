fcfeaf88dd401ffce93208fc5f422c7c9c60f1c1
Muhammed Can GEZGİN <93928479+Cangezgin1@users.noreply.github.com>
2024-02-20 15:19:36 +0300

    Update TestimonialViewModel.cs

--- Frontend/HotelProject.WebUI/Models/Testimonial/TestimonialViewModel.cs
+++ Frontend/HotelProject.WebUI/Models/Testimonial/TestimonialViewModel.cs
@@ -9,7 +9,7 @@ namespace HotelProject.WebUI.Models.Testimonial
     {
         public int TestimonialID { get; set; }
         public string Name { get; set; }
-        public string Title { get; set; }
+        public string Title { get; set; } 
         public string Description { get; set; }
         public string Image { get; set; }
     }
