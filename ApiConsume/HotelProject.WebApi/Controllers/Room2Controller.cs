f9bd9222aec5d46f6fff1e596122c932cf9f8aa4
Muhammed Can GEZGİN <93928479+Cangezgin1@users.noreply.github.com>
2024-06-04 13:23:06 +0300

    Update Room2Controller.cs

--- ApiConsume/HotelProject.WebApi/Controllers/Room2Controller.cs
+++ ApiConsume/HotelProject.WebApi/Controllers/Room2Controller.cs
@@ -14,7 +14,7 @@ namespace HotelProject.WebApi.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class Room2Controller : ControllerBase
-    {
+    { 
         private readonly IRoomService _roomService;
         private readonly IMapper _mapper;
 
